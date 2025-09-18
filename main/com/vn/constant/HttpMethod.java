a2655d288f4f7cb5181f6474ec2156f13e37afa3
quanglam04 <trinhquanglam2k4@gmail.com>
2025-09-18 15:09:57 +0700

    add constant

---
+++ main/com/vn/constant/HttpMethod.java
@@ -0,0 +1,8 @@
+package main.com.vn.constant;
+
+public class HttpMethod{
+  public static final String GET = "GET";
+  public static final String POST = "POST";
+  public static final String DELETE = "DELETE";
+  public static final String PUT = "PUT";
+}
---
+++ main/com/vn/constant/HttpStatus.java
@@ -0,0 +1,23 @@
+package main.com.vn.constant;
+
+public class HttpStatus {
+  public static final int OK = 200;
+  public static final int CREATED = 201;
+  public static final int ACCEPTED = 202;
+
+  public static final int NO_CONTENT = 204;
+
+  public static final int MOVED_PERMANENTLY = 301;
+  public static final int FOUND = 302;
+
+  public static final int BAD_REQUEST = 400;
+  public static final int UNAUTHORIZED = 401;
+  public static final int FORBIDDEN = 403;
+  public static final int NOT_FOUND = 404;
+
+  public static final int INTERNAL_SERVER_ERROR = 500;
+  public static final int NOT_IMPLEMENTED = 501;
+  public static final int BAD_GATEWAY = 502;
+  public static final int SERVICE_UNAVAILABLE = 503;
+
+}
